3c0114252c52fd138e002cbee593f528e22d582d
Drk <wldnro3141@gmail.com>
2020-04-03 16:56:30 +0900

    Remove NoArgsConstructor Annontation

--- src/main/java/com/drk/todolist/Entitis/ToDoListEntity.java
+++ src/main/java/com/drk/todolist/Entitis/ToDoListEntity.java
@@ -6,13 +6,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import lombok.AccessLevel;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @ToString
 @Getter
 @Setter
--- src/main/java/com/drk/todolist/Entitis/UserEntity.java
+++ src/main/java/com/drk/todolist/Entitis/UserEntity.java
@@ -11,9 +11,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 
-import lombok.AccessLevel;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
