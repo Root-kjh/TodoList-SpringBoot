8e8abb08eb669ae0f983852843fdadc3aa1fa3c8
Drk <wldnro3141@gmail.com>
2020-07-08 15:58:44 +0900

    add cors security settings

---
+++ src/main/java/com/drk/todolist/Config/Controller/Cors.java
@@ -0,0 +1,17 @@
+package com.drk.todolist.Config.Controller;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class Cors implements WebMvcConfigurer{
+        
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:3000")
+            .allowedMethods("*")
+            .exposedHeaders("X-AUTH-TOKEN");
+    }
+}
--- src/main/java/com/drk/todolist/Config/JWT/WebSecurityConfig.java
+++ src/main/java/com/drk/todolist/Config/JWT/WebSecurityConfig.java
@@ -9,6 +9,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import lombok.RequiredArgsConstructor;
 
@@ -40,8 +43,25 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
             .antMatchers("/resources/**").permitAll()
             .antMatchers("/**").authenticated()
             .and()
+            .cors()
+            .and()
             .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), 
                 UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.addAllowedOrigin("http://localhost:3000");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
     }
 
 }
